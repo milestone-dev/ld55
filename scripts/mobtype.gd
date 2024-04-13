08502222d8ba8c2ea1b135189944aebf2a436833
Patrik Sjöberg <voxxar@gmail.com>
2024-04-14 00:29:45 +0200

    mobtypes

---
+++ scripts/mobtype.gd
@@ -0,0 +1,15 @@
+extends Resource
+class_name Mobtype
+
+@export var max_hp = 100;
+@export var speed = 1000;
+@export var attack_damage = 5;
+@export var attack_cooldown_max = 0.3;
+@export var attack_range = 8;
+@export var exp : int = 5;
+@export var sprite: Texture2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
