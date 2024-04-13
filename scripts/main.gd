396220f9345dd0f92a7a63a12b1d4dce70477a3f
Patrik Sjöberg <voxxar@gmail.com>
2024-04-14 01:38:12 +0200

    Fixed viewport size for web

--- scripts/main.gd
+++ scripts/main.gd
@@ -37,7 +37,10 @@ func _screen_to_world(pos: Vector2) -> Vector2:
 
 func _random_new_mob_position() -> Vector2:
 	var margin = Vector2(25, 25)
-	var size = _screen_to_world(get_viewport_rect().size) / 2 + margin
+	#var size = _screen_to_world(get_viewport_rect().size)
+	#print("size", size)
+	# Use above to find the size to put here, checking veiwport seem to bug on web?
+	var size = Vector2(640, 400) / 2 + margin
 	var side = randi_range(0, 3)
 	var position = Vector2(randf() * size.x, randf() * size.y)*2 - size
 	if side == 0: position.x = -size.x;
