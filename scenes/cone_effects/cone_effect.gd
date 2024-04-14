de533bf022a06308c6d8f5c3651fd92bd181ea22
Patrik Sjöberg <voxxar@gmail.com>
2024-04-14 23:16:08 +0200

    flamethrower

--- scenes/cone_effects/cone_effect.gd
+++ scenes/cone_effects/cone_effect.gd
@@ -1,4 +1,4 @@
-@tool
+#@tool
 extends AreaOfEffect
 
 @export_range(0, 180) var angle: float = 45
@@ -11,11 +11,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var rad = deg_to_rad(angle)/2
-	$Area2D/CollisionPolygon2D.polygon[1].x = cos(rad)*range
-	$Area2D/CollisionPolygon2D.polygon[1].y = sin(rad)*range
-	$Area2D/CollisionPolygon2D.polygon[2].x = cos(TAU-rad)*range
-	$Area2D/CollisionPolygon2D.polygon[2].y = sin(TAU-rad)*range
-	var mat = $GPUParticles2D.process_material as ParticleProcessMaterial
-	mat.spread = angle / 2
-	$GPUParticles2D.lifetime = range/100;
+	if Engine.is_editor_hint():
+		var rad = deg_to_rad(angle)/2
+		$Area2D/CollisionPolygon2D.polygon[1].x = cos(rad)*range
+		$Area2D/CollisionPolygon2D.polygon[1].y = sin(rad)*range
+		$Area2D/CollisionPolygon2D.polygon[2].x = cos(TAU-rad)*range
+		$Area2D/CollisionPolygon2D.polygon[2].y = sin(TAU-rad)*range
+		var mat = $GPUParticles2D.process_material as ParticleProcessMaterial
+		mat.spread = angle / 2
+		$GPUParticles2D.lifetime = range/100;
+		return
+		
+	if look_at_pointer:
+		var pos = get_global_mouse_position()
+		look_at(pos)
+		
--- scripts/area_of_effect.gd
+++ scripts/area_of_effect.gd
@@ -2,11 +2,6 @@ extends Node2D
 class_name AreaOfEffect
 
 @export var spell : Spell
-@export var collider: CollisionObject2D
+@export var collider: Area2D
+@export var look_at_pointer: bool = false
 
-func _ready():
-	if collider:
-		collider.body_shape_entered.connect(_on_body_enter)
-
-func _on_body_enter(body):
-	print("hit ", body)
