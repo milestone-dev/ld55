cd390df7cc7801eb79efe67be8c1c77b83c52922
Patrik Sjöberg <voxxar@gmail.com>
2024-04-14 22:01:19 +0200

    cone effect

---
+++ scenes/cone_effects/cone_effect.gd
@@ -0,0 +1,21 @@
+@tool
+extends Node2D
+@export var collider: CollisionObject2D
+@export_range(0, 180) var angle: float = 45
+@export_range(10, 200) var range: float = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if not collider:
+		collider = $Area2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var rad = deg_to_rad(angle)/2
+	$Area2D/CollisionPolygon2D.polygon[1].x = cos(rad)*range
+	$Area2D/CollisionPolygon2D.polygon[1].y = sin(rad)*range
+	$Area2D/CollisionPolygon2D.polygon[2].x = cos(TAU-rad)*range
+	$Area2D/CollisionPolygon2D.polygon[2].y = sin(TAU-rad)*range
+	var mat = $GPUParticles2D.process_material as ParticleProcessMaterial
+	mat.spread = angle / 2
+	$GPUParticles2D.lifetime = range/100;
