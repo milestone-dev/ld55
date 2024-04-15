c281d658ed125feef9627ae499f0a0338a0d9b36
Milestone <56237952+milestone-dev@users.noreply.github.com>
2024-04-15 03:03:58 +0200

    Fixes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,8 +4,8 @@ extends Panel
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if $AnimationPlayer:
-		$AnimationPlayer.play("blink")
+	if has_node("AnimationPlayer"):
+		get_node("AnimationPlayer").play("blink")
 	if score_subtitle != null:
 		score_subtitle.text = "You survived for %d seconds and killed %d monsters" % [Global.time_survived, Global.mobs_killed];	
 	
